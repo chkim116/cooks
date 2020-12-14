699e0e8ae361cd2bb959b2e2f6227f9f554adb39
chkim116 <dudnqnfqlc@naver.com>
2020-12-15 03:35:02 +0900

    Feat useScroll & useMore

--- @event/lib/index.ts
+++ @event/lib/index.ts
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export const useFormInput = (initialValue?: object | undefined) => {
     const [form, setForm] = useState({});
@@ -35,16 +35,77 @@ export const useToggle = (boolean: boolean) => {
     return [toggle, onClick] as const;
 };
 
-export const useFindId = () => {
-    const [findId, setFindId] = useState<string | undefined>("");
+export const useFind = () => {
+    const [findData, setFindData] = useState<DOMStringMap>();
 
     const onClick = useCallback(
         (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-            const { id } = e.currentTarget.dataset;
-            setFindId(id);
+            setFindData(e.currentTarget.dataset);
         },
         []
     );
 
-    return [findId, onClick] as const;
+    return [findData, onClick] as const;
+};
+
+type Props = {
+    length: number;
+    initial: number;
+    count: number;
+    limit: number;
+};
+
+export const useScroll = (
+    viewPort: any,
+    { length, initial, count, limit }: Props,
+    query?: any,
+    isLoading?: boolean
+): any => {
+    const [display, setDisplay] = useState(initial);
+    const lastElement = useCallback(
+        (node) => {
+            if (
+                isLoading ||
+                length < display ||
+                display === limit ||
+                viewPort === undefined ||
+                node === null
+            )
+                return;
+            viewPort = new IntersectionObserver((entries) => {
+                if (entries[0].isIntersecting && display !== limit) {
+                    viewPort.disconnect();
+                    setDisplay((prev: number) => (prev += count));
+                }
+            });
+            if (node) {
+                viewPort.observe(node);
+            }
+        },
+        [isLoading, display, length, limit, viewPort]
+    );
+    useEffect(() => {
+        setDisplay((prev) => initial);
+    }, [query]);
+
+    return [lastElement, display];
+};
+
+export const useMore = (
+    { length, initial, count, limit }: Props,
+    query?: any,
+    isLoading?: boolean
+): any => {
+    const [display, setDisplay] = useState(initial);
+
+    const onClick = useCallback(() => {
+        if (display === limit || length < display || isLoading) return;
+        setDisplay((prev: number) => (prev += count));
+    }, [display, length, limit]);
+
+    useEffect(() => {
+        setDisplay((prev) => initial);
+    }, [query]);
+
+    return [onClick, display];
 };
