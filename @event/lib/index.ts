89576e1eb1724c1e7a69312eda58041c6217594b
kimchanghoe <cskim@veluga.kr>
2021-03-04 23:16:11 +0900

    velooks lunched

--- @event/lib/index.ts
+++ @event/lib/index.ts
@@ -1,138 +1,68 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react"
 
 export const useFormInput = () => {
-    const [form, setForm] = useState({});
+    const [form, setForm] = useState({})
     const onChange = useCallback(
         (e: React.FormEvent<HTMLFormElement | HTMLInputElement>) => {
-            const { name, value } = e.target as HTMLInputElement;
-            setForm({ ...form, [name]: value });
+            const { name, value } = e.target as HTMLInputElement
+            setForm({ ...form, [name]: value })
         },
         [form]
-    );
+    )
 
-    return [form, onChange, setForm] as const;
-};
+    return [form, onChange, setForm] as const
+}
 
-type InitialValue = string | number;
+type InitialValue = string | number
 
 export const useInput = (initialValue: InitialValue) => {
-    const [value, setValue] = useState<InitialValue>(initialValue);
+    const [value, setValue] = useState<InitialValue>(initialValue)
     const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-        const { value } = e.target;
-        setValue(value);
-    }, []);
+        const { value } = e.target
+        setValue(() => value)
+    }, [])
 
-    return [value, onChange, setValue] as const;
-};
+    return [value, onChange, setValue]
+}
 
 export const useToggle = (boolean: boolean) => {
-    const [toggle, setToggle] = useState<boolean>(boolean);
+    const [toggle, setToggle] = useState<boolean>(boolean)
 
     const onClick = useCallback(() => {
-        setToggle((prev: boolean) => !prev);
-    }, []);
+        setToggle((prev: boolean) => !prev)
+    }, [])
 
-    return [toggle, onClick] as const;
-};
+    return [toggle, onClick, setToggle]
+}
 
 export const useFind = () => {
-    const [findData, setFindData] = useState<DOMStringMap>();
+    const [findData, setFindData] = useState<DOMStringMap>()
 
     const onClick = useCallback(
         (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-            setFindData(e.currentTarget.dataset);
+            setFindData(() => e.currentTarget.dataset)
         },
         []
-    );
+    )
 
-    return [findData, onClick] as const;
-};
+    return [findData, onClick, setFindData]
+}
 
 export const useFindId = () => {
-    const [findId, setFindId] = useState<string | undefined>("");
+    const [findId, setFindId] = useState<string | undefined>("")
     const onClick = useCallback(
         (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-            const { id } = e.currentTarget.dataset;
-            setFindId(id);
+            const { id } = e.currentTarget.dataset
+            setFindId(() => id)
         },
         []
-    );
+    )
 
-    return [findId, onClick] as const;
-};
-
-type Props = {
-    viewPort?: any;
-    length: number;
-    initial: number;
-    count: number;
-    limit: number;
-    isLoading?: boolean;
-    query?: any;
-};
-
-export const useScroll = ({
-    viewPort,
-    length,
-    initial,
-    count,
-    limit,
-    query,
-    isLoading,
-}: Props): any => {
-    const [display, setDisplay] = useState(initial);
-    const lastElement = useCallback(
-        (node) => {
-            if (
-                isLoading ||
-                length < display ||
-                display === limit ||
-                viewPort === undefined ||
-                node === null
-            )
-                return;
-            viewPort = new IntersectionObserver((entries) => {
-                if (entries[0].isIntersecting && display !== limit) {
-                    viewPort.disconnect();
-                    setDisplay((prev: number) => (prev += count));
-                }
-            });
-            if (node) {
-                viewPort.observe(node);
-            }
-        },
-        [isLoading, display, length, limit, viewPort]
-    );
-    useEffect(() => {
-        setDisplay((prev) => initial);
-    }, [query]);
-
-    return [lastElement, display];
-};
-
-export const useMore = ({
-    length,
-    initial,
-    count,
-    limit,
-    query,
-    isLoading,
-}: Props): any => {
-    const [display, setDisplay] = useState(initial);
-    const onClick = useCallback(() => {
-        if (display === limit || length < display || isLoading) return;
-        setDisplay((prev: number) => (prev += count));
-    }, [display, length, limit]);
-
-    useEffect(() => {
-        setDisplay((prev) => initial);
-    }, [query]);
-
-    return [onClick, display];
-};
+    return [findId, onClick, setFindId]
+}
 
 export const useScrollTop = () => {
     useEffect(() => {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-    }, []);
-};
+        window.scrollTo({ top: 0, behavior: "smooth" })
+    }, [])
+}
--- @utils/lib/index.ts
+++ @utils/lib/index.ts
@@ -1,33 +1,35 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
 
 export const useReplace = (string: string) => {
-    const word = string.replace(/<[^>]*>?/gm, "");
+    const word = string.replace(/<[^>]*>?/gm, "")
 
-    return word;
-};
+    return word
+}
 
 export const useKtime = () => {
     const timestamp =
-        Date.now() + new Date().getTimezoneOffset() * 60000 + 9 * 3600 * 1000;
-    return new Date(timestamp);
-};
+        Date.now() + new Date().getTimezoneOffset() * 60000 + 9 * 3600 * 1000
+    return new Date(timestamp)
+}
 
 export const useClip = (e: any) => {
-    const [message, setMessage] = useState(false);
-    const text = document.createElement("textarea");
-    document.body.appendChild(text);
-    text.value = e.target.innerText;
-    text.select();
-    document.execCommand("copy");
-    document.body.removeChild(text);
-    setMessage(true);
+    const [message, setMessage] = useState(false)
+
+    const text = document.createElement("textarea")
+    document.body.appendChild(text)
+    text.value = e.target.innerText
+    text.select()
+    document.execCommand("copy")
+    document.body.removeChild(text)
+    setMessage(true)
+
     useEffect(() => {
         if (message) {
             setTimeout(() => {
-                setMessage(false);
-            }, 1000);
+                setMessage(false)
+            }, 1000)
         }
-    }, [message]);
+    }, [message])
 
-    return [message, setMessage];
-};
+    return [message, setMessage]
+}
